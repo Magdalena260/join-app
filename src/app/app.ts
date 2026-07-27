e511b760eabc072f18e39e381e768aa58f831541
Magdalena260 <magdalena.laurisch@outlook.de>
2026-07-27 14:40:50 +0200

    legal notice & privacy policy

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,8 @@ import { Header } from './layout/header/header';
 import { SidebarComponent } from './layout/sidebar/sidebar';
 import { RotateScreen } from './shared/components/rotate-screen/rotate-screen';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
