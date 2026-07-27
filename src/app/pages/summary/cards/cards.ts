7bc042ea863fc16254c00cd5b977c489fb64865e
Denis Nolting <denis.nolting@gmx.de>
2026-07-27 13:39:53 +0200

    style: responsive design

---
+++ src/app/pages/summary/cards/cards.ts
@@ -0,0 +1,75 @@
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Task } from '../../../shared/interfaces/tasks';
+import { tasksService } from '../../../shared/services/tasks-service';
+
+@Component({
+  selector: 'app-cards',
+  imports: [RouterLink],
+  templateUrl: './cards.html',
+  styleUrl: './cards.scss',
+})
+export class Cards  implements OnInit {
+  private tasksService = inject(tasksService);
+
+  todoTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 0).length;
+  });
+
+  progressTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 1).length;
+  });
+
+  feedbackTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 2).length;
+  });
+
+  doneTasksCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.status === 3).length;
+  });
+
+  tasksInBoard = computed(() => {
+    return this.todoTasksCount() + this.progressTasksCount() + this.feedbackTasksCount();
+  });
+
+  urgentTaskCount = computed(() => {
+    return this.tasksService.tasks().filter(task => task.priority === 2).length
+  })
+
+  closestDueTask = computed(() => {
+    const today = new Date().getTime();
+
+    return this.tasksService.tasks()
+      .filter(task => task.due_date)
+      .filter(task => new Date(task.due_date).getTime() >= today)
+      .reduce((closestTask, currentTask) => {
+        if (!closestTask) {
+          return currentTask;
+        }
+
+        const closestTime = new Date(closestTask.due_date).getTime();
+        const currentTime = new Date(currentTask.due_date).getTime();
+
+        return currentTime < closestTime ? currentTask : closestTask;
+      }, null as Task | null);
+  });
+
+  closestDueDate = computed(() => {
+    return this.closestDueTask()?.due_date ?? '';
+  });
+
+  async ngOnInit(): Promise<void> {
+    await this.tasksService.getTasks();
+    await this.tasksService.subscribeToTasks();
+  }
+
+  formatDateForSummary(date: string): string {
+    const parsedDate = new Date(date);
+
+    return parsedDate.toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  }
+}
--- src/app/pages/summary/summary.ts
+++ src/app/pages/summary/summary.ts
@@ -1,78 +1,13 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
-import { tasksService } from '../../shared/services/tasks-service';
-import { RouterLink } from '@angular/router';
-import { Task } from '../../shared/interfaces/tasks';
+import { Component } from '@angular/core';
 import { Greeting } from './greeting/greeting';
+import { Cards } from './cards/cards';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [RouterLink, Greeting],
+  imports: [Greeting, Cards],
   templateUrl: './summary.html',
   styleUrl: './summary.scss',
 })
 
-export class Summary implements OnInit {
-  private tasksService = inject(tasksService);
-
-  todoTasksCount = computed(() => {
-    return this.tasksService.tasks().filter(task => task.status === 0).length;
-  });
-
-  progressTasksCount = computed(() => {
-    return this.tasksService.tasks().filter(task => task.status === 1).length;
-  });
-
-  feedbackTasksCount = computed(() => {
-    return this.tasksService.tasks().filter(task => task.status === 2).length;
-  });
-
-  doneTasksCount = computed(() => {
-    return this.tasksService.tasks().filter(task => task.status === 3).length;
-  });
-
-  tasksInBoard = computed(() => {
-    return this.todoTasksCount() + this.progressTasksCount() + this.feedbackTasksCount();
-  });
-
-  urgentTaskCount = computed(() => {
-    return this.tasksService.tasks().filter(task => task.priority === 2).length
-  })
-
-  closestDueTask = computed(() => {
-    const today = new Date().getTime();
-
-    return this.tasksService.tasks()
-      .filter(task => task.due_date)
-      .filter(task => new Date(task.due_date).getTime() >= today)
-      .reduce((closestTask, currentTask) => {
-        if (!closestTask) {
-          return currentTask;
-        }
-
-        const closestTime = new Date(closestTask.due_date).getTime();
-        const currentTime = new Date(currentTask.due_date).getTime();
-
-        return currentTime < closestTime ? currentTask : closestTask;
-      }, null as Task | null);
-  });
-
-  closestDueDate = computed(() => {
-    return this.closestDueTask()?.due_date ?? '';
-  });
-
-  async ngOnInit(): Promise<void> {
-    await this.tasksService.getTasks();
-    await this.tasksService.subscribeToTasks();
-  }
-
-  formatDateForSummary(date: string): string {
-  const parsedDate = new Date(date);
-
-  return parsedDate.toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  });
-}
-}
+export class Summary {}
