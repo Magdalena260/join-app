023651b45c50b146f7332064d06f6c97d50dde14
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-07-27 11:32:13 +0200

    feat: add - Import SignUp component into routing config - Add 'sign-up' path to the application routes array

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AddTask } from './pages/add-task/add-task';
 import { Board } from './pages/board/board';
 import { Contacts } from './pages/contacts/contacts';
 import { Login } from './pages/login/login';
+import { SignUp } from './pages/sign-up/sign-up';
 
 export const routes: Routes = [
   { path: 'contacts', component: Contacts },
   { path: 'board', component: Board },
   { path: 'add-task', component: AddTask },
+  { path: 'sign-up', component: SignUp },
   { path: '', component: Login },
 ];
