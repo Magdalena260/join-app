1c70c867cfa34888bc779b9fb8a715e025ed71b2
Denis Nolting <denis.nolting@gmx.de>
2026-07-29 14:43:13 +0200

    feat: add routerlinks

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoDark } from '../../shared/components/logo-dark/logo-dark';
 import { ProfileIcon } from '../../shared/components/profile-icon/profile-icon';
-
-
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [LogoDark, ProfileIcon],
+  imports: [LogoDark, ProfileIcon, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
